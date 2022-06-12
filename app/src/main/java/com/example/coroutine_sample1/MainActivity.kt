fca865beb16cd3b1e846045122c8f4ac0d297267
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2022-06-12 11:42:26 +0900

    CoroutineScopeを継承した書き方に変更

--- app/src/main/java/com/example/coroutine_sample1/MainActivity.kt
+++ app/src/main/java/com/example/coroutine_sample1/MainActivity.kt
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity(
+    override val coroutineContext: CoroutineContext = Job() + Dispatchers.Main
+) : AppCompatActivity(), CoroutineScope {
 
-    private val scope = CoroutineScope(Job() + Dispatchers.Main)
+    // private val scope = CoroutineScope(Job() + Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        scope.launch {
+//        scope.launch {
+        launch {
             fetch()
         }
 
@@ -24,7 +28,8 @@ class MainActivity : AppCompatActivity() {
     private suspend fun fetch() {
         delay(1000L)
         Log.d("Tatsuyað", "fetch: 1秒経過")
-        scope.cancel()
+//        scope.cancel()
+        cancel()
         delay(1000L)
         Log.d("Tatsuyað", "fetch: 2秒経過")
     }
