675842610ac2952b6dd246df3a1991278ab2fa05
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2022-06-25 12:51:34 +0900

    launchを使った処理

--- app/src/main/java/com/example/coroutine_sample1/MainActivity.kt
+++ app/src/main/java/com/example/coroutine_sample1/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.coroutine_sample1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val scope = CoroutineScope(Job() + Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scope.launch {
+            val customName = customName("龍哉")
+            Log.d("Tatsuyað", "onCreate: ${customName}")
+        }
+
+    }
+
+    private suspend fun customName(name: String): String {
+        delay(1000)
+        return name + "さん"
     }
 }
